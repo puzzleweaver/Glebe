076783c869df708f5104ef298e9b5b23812d7f8f
Riley <rileyborgard@gmail.com>
2016-11-26 14:38:53 -0500

    Everything I've done so far- see description
    For directions, see the comment in client/index.html
    I'm not sure if you need to install node.js on your computer, but it
    can't hurt. You probably don't need to install libraries.
    I've made it so that each player is a character which moves by ASDW on a
    capture the flag board, where one half is red and the other half is
    blue.
    There is also a chat box which can be used to debug things if your
    message starts with "/"

---
+++ app.js
@@ -0,0 +1,135 @@
+
+var express = require("express");
+var app = express();
+var serv = require("http").Server(app);
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/client/index.html");
+});
+app.use("/client", express.static(__dirname + "/client"));
+
+serv.listen(2000);
+console.log("Server started.");
+
+var SOCKET_LIST = {};
+
+var Entity = function() {
+	var self = {
+		x:250,
+		y:250,
+		speedX: 0,
+		speedY: 0,
+		id:""
+	}
+	self.update = function() {
+		self.updatePosition();
+	}
+	self.updatePosition = function() {
+		self.x += self.speedX;
+		self.y += self.speedY;
+	}
+	return self;
+}
+
+var Player = function(id) {
+	var self = Entity();
+	self.id = id;
+	self.number = "" + Math.floor(10 * Math.random());
+	self.pressingLeft = false;
+	self.pressingRight = false;
+	self.pressingUp = false;
+	self.pressingDown = false;
+	self.maxSpeed = 10;
+	
+	var super_update = self.update;
+	self.update = function() {
+		self.updateSpeed();
+		super_update();
+	}
+	
+	self.updateSpeed = function() {
+		if(self.pressingLeft)
+			self.speedX = -self.maxSpeed;
+		else if(self.pressingRight)
+			self.speedX = self.maxSpeed;
+		else
+			self.speedX = 0;
+		
+		if(self.pressingUp)
+			self.speedY = -self.maxSpeed;
+		else if(self.pressingDown)
+			self.speedY = self.maxSpeed;
+		else
+			self.speedY = 0;
+	}
+	Player.list[id] = self;
+	return self;
+}
+Player.list = {};
+Player.onConnect = function(socket) {
+	var player = Player(socket.id);
+	socket.emit("id", socket.id);
+	socket.on("keyPress", function(data) {
+		if(data.inputId === "left")
+			player.pressingLeft = data.state;
+		else if(data.inputId === "right")
+			player.pressingRight = data.state;
+		else if(data.inputId === "up")
+			player.pressingUp = data.state;
+		else if(data.inputId === "down")
+			player.pressingDown = data.state;
+	});
+}
+Player.onDisconnect = function(socket) {
+	delete Player.list[socket.id];
+}
+Player.update = function() {
+	var pack = [];
+	for(var i in Player.list) {
+		var player = Player.list[i];
+		player.update();
+		pack.push({
+			x:player.x,
+			y:player.y,
+			id:player.id
+		});
+	}
+	return pack;
+}
+
+var DEBUG = true;
+
+var io = require("socket.io")(serv, {});
+io.sockets.on("connection", function(socket) {
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+	
+	Player.onConnect(socket);
+	
+	socket.on("disconnect", function() {
+		delete SOCKET_LIST[socket.id];
+		Player.onDisconnect(socket);
+	});
+	socket.on("sendMsgToServer", function(data) {
+		var playerName = ("" + socket.id).slice(2, 7);
+		for(var i in SOCKET_LIST) {
+			SOCKET_LIST[i].emit("addToChat", playerName + ": " + data);
+		}
+	});
+	socket.on("evalServer", function(data) {
+		if(!DEBUG)
+			return;
+		var res = eval(data);
+		socket.emit("evalAnswer", res);
+	});
+});
+
+setInterval(function() {
+	var pack = {
+		player:Player.update(),
+	}
+	for(var i in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.emit("newPositions", pack);
+	}
+}, 1000/25);
