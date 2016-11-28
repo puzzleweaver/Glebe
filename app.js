6da09eee8522e640e525cd4effd3ef57a2419f59
Riley <rileyborgard@gmail.com>
2016-11-27 20:32:13 -0500

    Made server accessible to other computers on local network

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
 });
 app.use("/client", express.static(__dirname + "/client"));
 
-serv.listen(2000);
+serv.listen(2000, "0.0.0.0");
 console.log("Server started.");
 
 var SOCKET_LIST = {};
